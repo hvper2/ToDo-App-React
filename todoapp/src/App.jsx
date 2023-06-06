3c85b626ecbc2e48c5c2b9db1ca2892c50698baa
Mariusz <mariusz.kwiecien2@onet.pl>
2023-06-06 14:22:17 +0200

    ToDo App

--- todoapp/src/App.jsx
+++ todoapp/src/App.jsx
@@ -1,28 +1,12 @@
 import './App.css';
-import { tasks as tasksData} from './data/tasks';
-import { useState } from 'react';
+import { TasksList } from './components/TasksList';
 
 function App() {
-    const [tasks, setTasks] = useState(tasksData)
-    const handleDeleteTask = (index) => {
-        const newTasks = [... tasks];
-        newTasks.splice(index , 1);
-        setTasks(newTasks);
-    }
+
   
     return(
         <div>
-            <ul>
-            {tasks.map((task,index) =>{
-                return (
-                    <>
-                    <li key={index}>{task.title}</li>
-                    <button onClick={() => handleDeleteTask(index)}>X</button>
-                    </>
-                )
-            }
-            )}
-            </ul>
+            <TasksList/>
         </div>
   )
 }
