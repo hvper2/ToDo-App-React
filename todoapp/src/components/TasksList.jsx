15ec9bcbb72884748666533cf6f4184867188a08
Mariusz <mariusz.kwiecien2@onet.pl>
2023-06-08 10:41:49 +0200

    Update TasksList.jsx

--- todoapp/src/components/TasksList.jsx
+++ todoapp/src/components/TasksList.jsx
@@ -1,12 +1,22 @@
-import { useState } from "react";
-import { tasks as tasksData } from "../data/tasks";
+import { useRef, useState } from "react";
 
 export function TasksList() {
-  const [tasks, setTasks] = useState(tasksData);
+  const [tasks, setTasks] = useState([]);
+  const titleRef = useRef(null);
 
-  const handleCompleteTask = (index) => {
+  const handleAddTask = () => {
     const newTasks = [...tasks];
-    newTasks[index].completed = true;
+    newTasks.push({
+      title: titleRef.current.value,
+      description: "Lorem ipsum X",
+      completed: false,
+    });
+    setTasks(newTasks);
+  };
+
+  const handleToggleTaskState = (index) => {
+    const newTasks = [...tasks];
+    newTasks[index].completed = !newTasks[index].completed;
     setTasks(newTasks);
   };
 
@@ -17,23 +27,33 @@ export function TasksList() {
   };
 
   return (
-    <ul>
-      {tasks.map((task, index) => {
-        return (
-          <>
-            <li
-              key={index}
-              style={{
-                textDecortacion: task.completed ? "line-through" : "none",
-              }}
-            >
-              {task.title}
-            </li>
-            <button onClick={() => handleCompleteTask(index)}>Complete</button>
-            <button onClick={() => handleDeleteTask(index)}>Delete</button>
-          </>
-        );
-      })}
-    </ul>
+    <>
+      <input type="text" id="title" ref={titleRef} />
+      <button onClick={handleAddTask}>AddNewTask</button>
+      {tasks.length == 0 ? (
+        <div>Empty Tasks List</div>
+      ) : (
+        <ul>
+          {tasks.map(({ title, completed }, index) => {
+            return (
+              <>
+                <li
+                  key={index}
+                  style={{
+                    textDecoration: completed ? "line-through" : "none",
+                  }}
+                >
+                  {title}
+                </li>
+                <button onClick={() => handleToggleTaskState(index)}>
+                  {completed ? "Undo" : "Complete"}
+                </button>
+                <button onClick={() => handleDeleteTask(index)}>Delete</button>
+              </>
+            );
+          })}
+        </ul>
+      )}
+    </>
   );
 }
